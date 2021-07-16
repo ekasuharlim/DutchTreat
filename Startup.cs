22ad8b323733b91cbe04ee294646461f7f5ef08e
eka <eka@example.com>
2021-07-16 09:36:48 +0800

    init code

---
+++ Startup.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace DutchTreat
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //app.Run(RequestHandler) or below command;
+            app.Run( context => {  return context.Response.WriteAsync("Hello eka 2"); });
+                     
+        }
+
+        public Task RequestHandler(HttpContext context) {
+
+            Thread.Sleep(5000);
+            return context.Response.WriteAsync("Hello Eka");
+        }
+    }
+}
