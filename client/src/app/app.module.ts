54c25184ce4447341f7782488ac418798574d3a8
eka <eka@example.com>
2021-08-18 09:22:53 +0800

    add first component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import ProductListView from './views/productListView.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+        AppComponent,
+        ProductListView
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/views/productListView.component.ts
@@ -0,0 +1,18 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector: "product-list",
+    templateUrl: "productListView.component.html"
+})
+export default class ProductListView {
+    public products = [{
+        title: "Van gogh mug",
+        price: "19"
+    }, {
+        title: "Vam gogh poster",
+        price: "3"
+    }
+    ]
+
+    public title = "a test property"
+}
